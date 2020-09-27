9139cf618a14e744b34a4a2834ba28c518e2a7a1
AycanOcal <aycanocal28@gmail.com>
2020-09-27 18:27:14 +0300

    Add author feature

--- src/main/java/com/spring/library/controller/LibraryController.java
+++ src/main/java/com/spring/library/controller/LibraryController.java
@@ -1,11 +1,17 @@
 package com.spring.library.controller;
 
+import com.spring.library.entity.Author;
+import com.spring.library.entity.Book;
+import com.spring.library.repository.AuthorRepository;
 import com.spring.library.service.AuthorService;
 import com.spring.library.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LibraryController {
@@ -15,6 +21,7 @@ public class LibraryController {
 
     @Autowired
     BookService bookService;
+
 
     @GetMapping("/")
     public String viewHomePage(Model model) {
@@ -36,4 +43,18 @@ public class LibraryController {
             model.addAttribute("books", bookService.getAllBooks());
         return "books";
     }
+
+    @GetMapping("/addAuthor")
+    public String showAuthorForm(Model model) {
+        Author author = new Author();
+        model.addAttribute("author", author);
+        return "add-author-form";
+    }
+
+    @PostMapping("/saveAuthor")
+    public String saveAuthor(@ModelAttribute("author") Author author) {
+        authorService.addAuthor(author);
+        return "redirect:/authorsPage";
+    }
+
 }
--- src/main/java/com/spring/library/repository/BookRepository.java
+++ src/main/java/com/spring/library/repository/BookRepository.java
@@ -1,6 +1,5 @@
 package com.spring.library.repository;
 
-import com.spring.library.entity.Author;
 import com.spring.library.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,7 +7,6 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
