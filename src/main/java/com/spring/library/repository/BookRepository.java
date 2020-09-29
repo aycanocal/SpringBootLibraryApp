00a76384955bf651700d93133894a53fa64920b9
AycanOcal <aycanocal28@gmail.com>
2020-09-29 05:02:04 +0300

    Search update

--- src/main/java/com/spring/library/repository/BookRepository.java
+++ src/main/java/com/spring/library/repository/BookRepository.java
@@ -11,6 +11,6 @@ import java.util.List;
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-    @Query(value="select * from book b where b.book_name like %:keyword% or b.book_description like %:keyword% or b.isbn like %:keyword% or b.book_sub_name like %:keyword% or b.book_serial_name like %:keyword%", nativeQuery = true)
+    @Query(value="select * from book b inner join author a on (b.author_id = a.author_id) inner join publisher p on (b.publisher_id = p.publisher_id)  where b.book_name like %:keyword% or b.book_description like %:keyword% or b.isbn like %:keyword% or b.book_sub_name like %:keyword% or b.book_serial_name like %:keyword% or a.author_name like %:keyword% or p.publisher_name like %:keyword%", nativeQuery = true)
     List<Book> findByKeyword(@Param("keyword") String keyword);
 }
